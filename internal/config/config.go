70360ee641d3d345d1919248fb1b54d6dca2a5bf
MaysHroub <hroubmays@gmail.com>
2025-08-09 19:24:02 +0300

    feat: implement ReadConfig function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,29 @@
 // Package config provides configuration structures and functions for the application.
 package config
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+const ConfigFileName = ".gatorconfig.json"
+
 type Config struct {
 	DatabaseURL     string `json:"db_url"`
 	CurrentUsername string `json:"current_user_name"`
 }
 
 func ReadConfig(path string) (Config, error) {
-	return Config{}, nil
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var config Config
+	if err = json.Unmarshal(data, &config); err != nil {
+		return Config{}, err
+	}
+	return config, nil
 }
+
