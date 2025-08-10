055319ffae3e7785ac48863062f75c9c7e053c1a
MaysHroub <hroubmays@gmail.com>
2025-08-10 18:27:13 +0300

    test(cmd): add unit test for valid state creation

--- cmd/commands_test.go
+++ cmd/commands_test.go
@@ -1,15 +1,31 @@
 package cmd
 
 import (
+	"github/MaysHroub/gator/internal/config"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestCreatingCommandWithParseCliArgs(t *testing.T) {
+func TestCreatingCommandWithParseCliArgs_ValidParsing(t *testing.T) {
 	args := []string{"login", "username"}
 	cmd := ParseCliArgs(args...)
 
 	assert.Equal(t, "login", cmd.name)
 	assert.Equal(t, []string{"username"}, cmd.args)
+}
+
+func TestStateCreation_ValidCreation(t *testing.T) {
+	path := filepath.Join(t.TempDir(), "statetest.json")
+	cfgService, err := config.NewConfigService(path)
+	require.NoError(t, err)
+	
+	cfgService.SetUser("mays-alreem")
+	state := state {
+		cfgService: &cfgService,
+	}
+
+	assert.Equal(t, cfgService, state.cfgService)
 }
