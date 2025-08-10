703103b602d324663d49fa6b6eb7aacf12035020
MaysHroub <hroubmays@gmail.com>
2025-08-10 19:07:47 +0300

    feat: create commands struct and its methods to pass the test

---
+++ cmd/command_registry.go
@@ -0,0 +1,26 @@
+package cmd
+
+import "fmt"
+
+type commands struct {
+	cmds map[string]func(*state, command) error
+}
+
+func NewCommands() commands {
+	mp := make(map[string]func(*state, command) error)
+	return commands{
+		cmds: mp,
+	}
+}
+
+func (c *commands) register(cmdName string, cmdHandler func(*state, command) error) {
+	c.cmds[cmdName] = cmdHandler
+}
+
+func (c *commands) run(st *state, cmd command) error {
+	handler, exists := c.cmds[cmd.name]
+	if !exists {
+		return fmt.Errorf("no such command exists: %s", cmd.name)
+	}
+	return handler(st, cmd)
+}
