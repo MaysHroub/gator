a5b4889fad81da32605c29cf5ee3b985af16aa90
MaysHroub <hroubmays@gmail.com>
2025-08-14 18:59:26 +0300

    feat(feeds): add up and down migrations for feeds table creation

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,10 +1,10 @@
 -- +goose Up
-create table users(
-    id UUID primary key,
-    created_at timestamp not null,
-    updated_at timestamp not null,
-    name text unique not null
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT UNIQUE NOT NULL
 );
 
 -- +goose Down
-drop table users;
+DROP TABLE users;
