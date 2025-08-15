875a451adb8e30c76fc39468a0026a44706f878e
MaysHroub <hroubmays@gmail.com>
2025-08-15 16:52:43 +0300

    feat(follow): add sql query to insert feed follow records and generate go code for it

---
+++ sql/queries/following.sql
@@ -0,0 +1,28 @@
+-- name: CreateFeedFollow :many
+WITH
+    inserted_feed_follow AS (
+        INSERT INTO
+            feed_follows (id, created_at, updated_at, user_id, feed_id)
+        VALUES
+            ($1, $2, $3, $4, $5)
+        RETURNING
+*
+    )
+SELECT
+    iff.*,
+    f.name AS feed_name,
+    u.name AS user_name
+FROM
+    inserted_feed_follow iff
+    JOIN users u ON u.id=iff.user_if
+    JOIN feeds f ON f.id=iff.feed_id
+GROUP BY
+    iff.id;
+
+-- name: GetFeedByUrl :one
+SELECT
+    *
+FROM
+    feeds
+WHERE
+    feeds.url=$1;
