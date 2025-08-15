80985e2bca609f356a9af119a0ed2e8646c678d3
MaysHroub <hroubmays@gmail.com>
2025-08-15 19:20:33 +0300

    feat(follows): add query to return feed follows for given user

--- internal/database/following.sql.go
+++ internal/database/following.sql.go
@@ -19,7 +19,8 @@ WITH
             feed_follows (id, created_at, updated_at, user_id, feed_id)
         VALUES
             ($1, $2, $3, $4, $5)
-        RETURNING id, created_at, updated_at, user_id, feed_id
+        RETURNING
+id, created_at, updated_at, user_id, feed_id
     )
 SELECT
     iff.id, iff.created_at, iff.updated_at, iff.user_id, iff.feed_id,
@@ -74,6 +75,61 @@ func (q *Queries) CreateFeedFollow(ctx context.Context, arg CreateFeedFollowPara
 			&i.FeedID,
 			&i.FeedName,
 			&i.UserName,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const getFeedFollowsForUser = `-- name: GetFeedFollowsForUser :many
+SELECT
+    ff.id, ff.created_at, ff.updated_at, user_id, feed_id, u.id, u.created_at, u.updated_at, name
+FROM
+    feed_follows ff
+    JOIN users u ON u.id=ff.user_id
+WHERE
+    u.name=$1
+`
+
+type GetFeedFollowsForUserRow struct {
+	ID          uuid.UUID
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	UserID      uuid.UUID
+	FeedID      uuid.UUID
+	ID_2        uuid.UUID
+	CreatedAt_2 time.Time
+	UpdatedAt_2 time.Time
+	Name        string
+}
+
+func (q *Queries) GetFeedFollowsForUser(ctx context.Context, name string) ([]GetFeedFollowsForUserRow, error) {
+	rows, err := q.db.QueryContext(ctx, getFeedFollowsForUser, name)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetFeedFollowsForUserRow
+	for rows.Next() {
+		var i GetFeedFollowsForUserRow
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.UserID,
+			&i.FeedID,
+			&i.ID_2,
+			&i.CreatedAt_2,
+			&i.UpdatedAt_2,
+			&i.Name,
 		); err != nil {
 			return nil, err
 		}
--- sql/queries/following.sql
+++ sql/queries/following.sql
@@ -5,7 +5,8 @@ WITH
             feed_follows (id, created_at, updated_at, user_id, feed_id)
         VALUES
             ($1, $2, $3, $4, $5)
-        RETURNING *
+        RETURNING
+*
     )
 SELECT
     iff.*,
@@ -17,3 +18,12 @@ FROM
     JOIN feeds f ON f.id=iff.feed_id
 GROUP BY
     iff.id;
+
+-- name: GetFeedFollowsForUser :many
+SELECT
+    *
+FROM
+    feed_follows ff
+    JOIN users u ON u.id=ff.user_id
+WHERE
+    u.name=$1;
