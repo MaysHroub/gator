18662d3c83eae59b3b1af80dfc3a07a91dcd70ee
MaysHroub <hroubmays@gmail.com>
2025-08-18 11:35:05 +0300

    chore(cmd): move all command logic to command.go

--- cmd/command.go
+++ cmd/command.go
@@ -1,6 +1,30 @@
 // Package cmd provides command-line argument parsing and executing utilities.
 package cmd
 
+import "fmt"
+
+type Commands struct {
+	cmds map[string]commandInfo
+}
+
+func NewCommands() Commands {
+	return Commands{
+		cmds: make(map[string]commandInfo),
+	}
+}
+
+func (c *Commands) Register(cmdName string, cmdInfo commandInfo) {
+	c.cmds[cmdName] = cmdInfo
+}
+
+func (c *Commands) Run(st *State, cmd Command) error {
+	cmdDetails, exists := c.cmds[cmd.name]
+	if !exists {
+		return fmt.Errorf("no such command exists: %s", cmd.name)
+	}
+	return cmdDetails.handler(st, cmd)
+}
+
 type Command struct {
 	name string
 	args []string
